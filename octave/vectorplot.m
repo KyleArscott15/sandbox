4ccb69eb5a551c0d38047d69cc227094699c8adb
Kyle Arscott <arscotkm@mcmaster.ca>
2021-12-18 11:57:14 -0500

    Octave: add cool vector plot with convenience options like enabling rotation, and setting window size.

---
+++ octave/vectorplot.m
@@ -0,0 +1,22 @@
+clf;
+ 
+ #colormap ("default");
+ 
+ startx = 100;
+ starty = 100;
+ width = 1200;
+ height = 800;
+ figure(1, 'position',[startx,starty,width,height]);
+ 
+ [x, y, z] = peaks (25);
+ 
+ surf (x, y, z);
+ hold on;
+ [u, v, w] = surfnorm (x, y, z / 10);
+ h = quiver3 (x, y, z, u, v, w);
+ set (h, "maxheadsize", 0.2);
+ hold off;
+ %shading interp;
+ title ("quiver3 of surface normals to peaks() function");
+ 
+ rotate3d on
